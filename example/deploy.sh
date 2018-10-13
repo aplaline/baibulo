b2b5c46d5062c6e7b895665a73c47c0e6889f20b
Matthias Hryniszak <padcom@gmail.com>
2018-10-13 16:22:12 +0200

    Updated example application to make the demo easier

--- example/deploy.sh
+++ example/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-npx baibulo-deploy deploy --url http://localhost:3000 --version next --dir hello
+npx baibulo-deploy deploy --url http://localhost:3000 --version 1 --dir hello-1
+npx baibulo-deploy deploy --url http://localhost:3000 --version 2 --dir hello-2
--- example/release.sh
+++ example/release.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-npx baibulo-deploy deploy --url http://localhost:3000 --version release --dir hello
+npx baibulo-deploy deploy --url http://localhost:3000 --version release --dir hello-release
